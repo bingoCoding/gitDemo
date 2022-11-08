3d1fec5b9dbc49325733b728290661ef0eef4a16
bingoCoding <1341919993@qq.com>
2022-11-08 15:22:23 +0800

    test 1

---
+++ src/main/java/com/example/gitdemo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.gitdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: TestController
+ * @Author: supsky
+ * @Date: 2022-11-08
+ **/
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test(){
+        return "test";
+    }
+}
